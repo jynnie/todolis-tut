d8aac836d900fb72d721e9ccad1d5c372254b343
Jynnie <jynnie@mit.edu>
2017-06-05 18:43:02 -0400

    Part Two: Views and Templating

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 app.run(debug=True, port=5000)
